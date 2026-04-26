323ca00f209c4aa09fed39916040c4c26ac918b3
anton <anton@lundstrom.nix>
2026-04-26 16:17:10 +0200

    host001

--- nixos/hosts/001/configuration.nix
+++ nixos/hosts/001/configuration.nix
@@ -44,24 +44,26 @@
       device = "/dev/disk/by-id/nvme-SAMSUNG_MZVLW128HEGR-000L1_S341NX1K479197";
       content = {
         type = "gpt";
-        ESP = {
-          size = "1G";
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
+        partitions = {
+          ESP = {
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
           };
-        };
-        swap = {
-          size = "4G";
-          content.type = "swap";
-        };
-        zfs = {
-          size = "100%";
-          content = {
-            type = "zfs";
-            pool = "rpool";
+          swap = {
+            size = "4G";
+            content.type = "swap";
+          };
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "rpool";
+            };
           };
         };
       };
