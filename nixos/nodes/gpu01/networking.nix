c94af4fc9c3996ae4dde33cd04036ee849e0c862
anton <anton@lundstrom.nix>
2026-05-18 11:42:06 +0200

    gpu01

--- nixos/nodes/gpu01/networking.nix
+++ nixos/nodes/gpu01/networking.nix
@@ -1,3 +1,19 @@
-{
+{...}: let
+  lan = import ../../settings/networking/configuration.nix;
+in {
   networking.hostName = "gpu01";
+  networking.useNetworkd = true;
+  networking.useDHCP = false;
+
+  systemd.network = {
+    enable = true;
+    networks."10-lan" = {
+      matchConfig.Name = "enp39s0";
+      address = ["${lan.nodes.gpu01.ip}/24"];
+      gateway = [lan.gateway];
+      dns = ["1.1.1.1" "9.9.9.9"];
+      networkConfig.IPv6AcceptRA = true;
+      linkConfig.RequiredForOnline = "routable";
+    };
+  };
 }
